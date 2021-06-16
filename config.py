ff95990a94304ef696110584957127cfa98e6a47
illusion999 <79356567+illusion999@users.noreply.github.com>
2021-06-17 02:32:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@
 
 api_id = "2943069"
 api_hash = "7ba2c6c7a6013f79ef95baab7dba92e3"
-bot_token = "1807849773:AAFZIum1SxeGjkkE0yNmNb5i1HiV0fdErQw"
+bot_token = "1761490515:AAEqjaZ51_hWgFM6k_KP4nc6Mi5MC3y1uxk"
