b46dcb3efe73c82837ecbb9b00612e3a35cd380b
illusion999 <79356567+illusion999@users.noreply.github.com>
2021-06-17 10:12:58 +0530

    Update start_message.py

--- src/start_message.py
+++ src/start_message.py
@@ -13,12 +13,11 @@ def start_message(client, message):
         [InlineKeyboardButton("Instructions", callback_data="instructions")],
     ]
     reply_markup = InlineKeyboardMarkup(kkeeyyb)
-    pic_url = "https://drive.google.com/file/d/1TzmuSj05qF6Sm5Z_CSm3AGUCBx4Oe0F0/view?usp=sharing"
+    pic_url = "https://drive.google.com/file/d/1DmfpdV43PHg5sRyP3yrIrAcfs_T_rvZi/view?usp=drivesdk"
     message.reply_photo(pic_url, caption=f"""**Hi {message.chat.first_name}**,
 
-Welcome to Anime DL Bot, Here you can Download all Anime for FREE 😁 ,For TG Anime Join @Anime_Collectors 
-!!!
+Konnichiwa senpai!
 
-__Please read all the instructions about the bot before surfing on...__
+Please read all the instructions about the bot before surfing on...__
 
 See /whats_new to know about latest updates...""", reply_markup=reply_markup, parse_mode="markdown")
